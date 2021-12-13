4b1e388d2a349ca21ae6f12d8f6cfe13b0e41548
rmffo <19081010050@upnjatim.ac.id>
2021-12-13 19:34:03 +0700

    refactor user getby route

--- application/config/routes.php
+++ application/config/routes.php
@@ -54,4 +54,4 @@ $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
 $route['api/user/all'] = '_API_User/getall';
-$route['api/user/by/(:any)/(:any)'] = '_API_User/getby/$1/$2';
+$route['api/user/(:any)/(:any)'] = '_API_User/getby/$1/$2';
